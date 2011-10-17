856a96b347ab336912be788c8a21095387ac2e78
Akos Palfi <palfia@homejinni.com>
2011-10-17 12:28:51 +0200

    [MIPS crankshaft merge] Fix evaluation order of GT and LTE operators.
    Ported commits:
    2876c377 (r9641)

--- src/mips/lithium-mips.cc
+++ src/mips/lithium-mips.cc
@@ -1403,13 +1403,11 @@ LInstruction* LChunkBuilder::DoPower(HPower* instr) {
 
 
 LInstruction* LChunkBuilder::DoCompareGeneric(HCompareGeneric* instr) {
-  Token::Value op = instr->token();
   Representation r = instr->GetInputRepresentation();
   ASSERT(instr->left()->representation().IsTagged());
   ASSERT(instr->right()->representation().IsTagged());
-  bool reversed = (op == Token::GT || op == Token::LTE);
-  LOperand* left = UseFixed(instr->left(), reversed ? a0 : a1);
-  LOperand* right = UseFixed(instr->right(), reversed ? a1 : a0);
+  LOperand* left = UseFixed(instr->left(), a1);
+  LOperand* right = UseFixed(instr->right(), a0);
   LCmpT* result = new LCmpT(left, right);
   return MarkAsCall(DefineFixed(result, v0), instr);
 }
