5a7d77c6371b59d42cd45e108edf9812fa9f7c4c
Akos Palfi <palfia@homejinni.com>
2011-10-20 19:51:20 +0200

    [MIPS crankshaft merge] Shave 39% from snapshot size.
    Ported commits:
    8ef3cfa4 (r9722)

--- src/mips/deoptimizer-mips.cc
+++ src/mips/deoptimizer-mips.cc
@@ -760,8 +760,9 @@ void Deoptimizer::EntryGenerator::Generate() {
   }
 
   // Set up the roots register.
-  ExternalReference roots_address = ExternalReference::roots_address(isolate);
-  __ li(roots, Operand(roots_address));
+  ExternalReference roots_array_start =
+      ExternalReference::roots_array_start(isolate);
+  __ li(roots, Operand(roots_array_start));
 
   __ pop(at);  // Get continuation, leave pc on stack.
   __ pop(ra);
